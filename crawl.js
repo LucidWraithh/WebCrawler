d829f4c79ae8a17f2c924e1797ffaf08ef3fe221
LucidWraithh <razor0916@gmail.com>
2024-04-25 14:33:14 -0500

    Created the first iteration of the crawler, which gets all web links in a parent url and counts how many link go to what website from its children.

--- crawl.js
+++ crawl.js
@@ -21,7 +21,7 @@ function getURLsFromHTML(html, baseURL){
     const jsdobj = new JSDOM(html)
     const anchorsFound = jsdobj.window.document.querySelectorAll('a')
 
-    for (anchor of anchorsFound) {
+    for (const anchor of anchorsFound) {
         strAnchor = String(anchor)
 
         if (strAnchor[0] === "/") {
@@ -36,9 +36,71 @@ function getURLsFromHTML(html, baseURL){
 }
 
 
-getURLsFromHTML()
+async function crawlPage(baseURL, currentURL=baseURL, pages={}) {
+    
+
+    if (!currentURL.includes(baseURL)) {
+        return pages
+    }
+
+    normalized = normalizeURL(currentURL)
+
+    if (pages[normalized] > 0) {
+        pages[normalized]++
+        return pages
+    }   else {
+        pages[normalized] = 1
+    }
+
+    console.log(`Crawling page ${currentURL}`)
+
+    let htmltext
+    try {
+
+        htmltext = await getWebpageText(currentURL)
+
+    } catch (error) {
+        console.log(error.message)
+        return pages
+    }
+
+
+    const newurls = getURLsFromHTML(htmltext, baseURL)
+
+    for (const link of newurls) {
+        pages = await crawlPage(baseURL, currentURL=link, pages=pages)
+    }
+
+    return pages
+}
+
+
+async function getWebpageText(currentURL) {
+    let resp
+
+    try {
+        resp = await fetch(currentURL, {
+            method: "GET",
+            mode: "cors"
+        })
+    } catch (error) {
+        throw new Error(`ERROR: Failed to get url; URL: ${currentURL}`)
+    }
+    
+    if (resp.status >= 400) {
+        console.log("ERROR: Web page malfunction")
+        return
+    }
+    
+    if (!resp.headers.get('content-type') || !resp.headers.get('content-type').includes("text/html")) {
+        console.log("ERROR: Content type is not in HTML")
+    }
+
+    return await resp.text()
+}
 
 module.exports = {
     normalizeURL,
-    getURLsFromHTML
+    getURLsFromHTML,
+    crawlPage
 }
--- main.js
+++ main.js
@@ -1,11 +1,21 @@
-function main() {
-    args = process.argv()
-    console.log(args)
-    /*if (args.length > 1) {
+const { crawlPage } = require('./crawl');
 
+async function main() {
+    const { argv } = require('node:process');
+
+    // print process.argv
+    args = argv.slice(2)
+    pages = {}
+
+    if (args.length > 1) {
+        console.log("too many inputs")
     } else if (args.length < 1) {
-
+        console.log("No inputs detected")
     }   else {
+        pages = await crawlPage(args[0])
+    }
 
-    }*/
+    console.log(pages)
 }
+
+main()
